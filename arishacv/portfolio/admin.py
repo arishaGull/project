623c4a7a6501920b6e3cee1e79763a5c459aae58
arishaGull <arishagull434@gmail.com>
2024-07-03 22:44:25 +0500

    Delete arishacv directory

--- arishacv/portfolio/admin.py
+++
@@ -1,9 +0,0 @@
-# portfolio/admin.py
-from django.contrib import admin
-from .models import Education, Skill, Experience, Project, Contact
-
-admin.site.register(Education)
-admin.site.register(Skill)
-admin.site.register(Experience)
-admin.site.register(Project)
-admin.site.register(Contact)
